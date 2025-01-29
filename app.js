5333942b31a994209accd10d6a1263594a18c445
Vando-Rocha <vandorcdev@gmail.com>
2025-01-28 21:18:00 -0300

    mostrando lista

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let listaAmigo = []
+let listaAmigo = [];
 
 function adicionarAmigo() {
     let amigoAdicionado = document.getElementById('amigo').value;
-    if(amigoAdicionado){
-        if(listaAmigo.includes(amigoAdicionado)){
-            alert("Amigo já esta na lista");
-        } else{
+    if (amigoAdicionado) {
+        if (listaAmigo.includes(amigoAdicionado)) {
+            alert("Amigo já está na lista");
+        } else {
             listaAmigo.push(amigoAdicionado);
             mostrarNomes(amigoAdicionado);
         }
-    }else{
-        alert("Por favor, insira um nome válido!")
+    } else {
+        alert("Por favor, insira um nome válido!");
     }
     console.log(listaAmigo);
 }
 
+function mostrarNomes(nomeAmigo) {
 
-function mostrarNomes(nomeAmigo){
-   let nome = document.querySelector('ul');
-   nome.innerHTML(`<li>${nomeAmigo}</li>`)
+    let lista = document.getElementById('listaAmigos');
+    let novoItem = document.createElement('li');
+    novoItem.textContent = nomeAmigo;
+
+    lista.appendChild(novoItem);
 }
+
+
+function sortearAmigo() {
+    if (listaAmigo.length === 0) {
+        alert("A lista está vazia! Adicione amigos antes de realizar o sorteio.");
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * listaAmigo.length);
+    let amigoSorteado = listaAmigo[indiceSorteado];
+
+    let resultado = document.getElementById('resultado');
+    let novoResultado = document.createElement('li');
+    novoResultado.textContent = resultado;
+}
+
