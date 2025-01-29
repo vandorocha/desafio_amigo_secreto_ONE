cd87c582ebbd49c1dd48fd09d65d7a176ea70590
Vando-Rocha <vandorcdev@gmail.com>
2025-01-28 21:02:54 -0300

    Lista implementada

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigo = []
+
+function adicionarAmigo() {
+    let amigoAdicionado = document.getElementById('amigo').value;
+    if(amigoAdicionado){
+        if(listaAmigo.includes(amigoAdicionado)){
+            alert("Amigo já esta na lista");
+        } else{
+            listaAmigo.push(amigoAdicionado);
+            mostrarNomes(amigoAdicionado);
+        }
+    }else{
+        alert("Por favor, insira um nome válido!")
+    }
+    console.log(listaAmigo);
+}
+
+
+function mostrarNomes(nomeAmigo){
+   let nome = document.querySelector('ul');
+   nome.innerHTML(`<li>${nomeAmigo}</li>`)
+}
